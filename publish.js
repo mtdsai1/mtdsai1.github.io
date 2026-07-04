3704db0e0617aa44a12b7020253ca935f17f6214
mtdsai1 <mtdsai222@gmail.com>
2026-07-04 10:46:25 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'brjrpocfh.cc/', 
+	'bcsbsofvr.cc/', 
 	'bcsbsofvr.cc/', 
     'bcsbsofvr.cc/',
 ];                                                                                                                  
