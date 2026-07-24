0f9a79100727da09eb5edb08796e5c97c9b84fcd
mtdsai1 <mtdsai222@gmail.com>
2026-07-24 18:20:59 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[
-	'opjtgufs.cc/', 
+	'hyruzbuv.cc/', 
 	'bcsbsofvr.cc/', 
     'bcsbsofvr.cc/',
 ];                                                                                                                  
 
-var JumpPage="https://mtds3.net";
+var JumpPage="https://mtds4.net";
 
 var newestUrls = [];
 
