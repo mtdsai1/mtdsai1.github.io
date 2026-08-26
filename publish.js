e4e821c7cac243b802a2ed82616d3f51d6bec260
mtdsai1 <mtdsai222@gmail.com>
2026-08-26 11:25:33 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -39,7 +39,7 @@ var emails = [
 var urls=[
 	'pvxgkunw.cc/', 
 	'cporahulr.com', 
-    'ibjcjgkd.cc/',
+    'njjipwie.cc/',
 ];                                                                                                                  
 
 var JumpPage="https://mtds7.net";
